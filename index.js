a3967d02555ac306d52677191e05c7c627d5f1df
Jeremy Antoine <jeremincanada@gmail.com>
2021-12-01 11:06:20 -0500

    sounds playing perfect

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
+// TO DO When pressing the touch : change the appearance and the sound done
 // TO DO On click change the appearance
 // TO DO On click add the sound
-// TO DO When pressing the touch : change the appearance and the sound
+
 
 document.addEventListener('keydown', function(e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-
   if(!audio) return; // if a key is not associated with a sound, function stops
-  audio.play(); //if I type on a key sounds come out but if I type twice it does not work
-  // the sound has to find its 'finish line to be activated again'
-
-
+  audio.currentTime = 0; // doesnt wait for the sound to finish, it rewinds automatically
+  audio.play(); //can press multiple times, the sounds plays from the beginning
 });
 
